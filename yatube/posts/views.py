b7e13323dfc1cef352929fd3396202bb51e2dba7
Piotr Shopin <mail4dev2022@gmail.com>
2022-10-08 12:34:47 +0200

    все тесты пройдены, произведена доработка по комментариям ревьюера №1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -64,34 +64,46 @@ def post_detail(request, post_id):
     author_name = post.author.get_full_name
     username = post.author.username
     posts_count = Post.objects.filter(author=post.author).count()
+    user_posts_link = 'profile/' + username
     context = {
         'post': post,
         'author_name': author_name,
         'username': username,
         'posts_count': posts_count,
+        'user_posts_link': user_posts_link,
     }
     return render(request, template, context)
 
 
 @login_required
-def post_create(request):
-    
+def post_create(request):    
     form = PostForm(request.POST or None)
+    template = 'posts/create_post.html'
+    context = {
+        'form': form
+        }
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
         post.save()
         return redirect('posts:profile', request.user)
-    return render(request, 'posts/create_post.html', {'form': form})
-
+    return render(request, template, context)
 
 
 @login_required
 def post_edit(request, post_id):
     is_edit = True
-    post = get_object_or_404(Post, pk=post_id)    
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id)
     form = PostForm(request.POST or None, instance=post)
+    template = 'posts/create_post.html'
+    context = {
+        'form': form,
+        'is_edit': is_edit,
+        'post_id': post_id,
+        }
     if form.is_valid():
         form.save()
         return redirect('posts:post_detail', post_id)
-    return render(request, 'posts/create_post.html', {'form': form, 'is_edit': is_edit })
+    return render(request, template, context)
